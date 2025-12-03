0b016d0560cc79a401e1a7eb009ddd0ada456f24
YangFeiyang <1966726811@qq.com>
2025-12-03 22:09:15 +0800

    Delete project/沙威玛传奇 directory

--- project/沙威玛传奇/沙威玛传奇/head.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include"gamestate.h"
-#include"playgame.h"
-#include"customers.h"
-extern void menu_main(GameState& state);
-extern void start_new_day(GameState& state);
-extern void upgrade_store(GameState& state);
